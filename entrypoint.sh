d5ff911bf1b99270ed229d568db580aa01c80761
nuke.blue <root@nuke.blue>
2025-07-13 22:03:34 +0000

    Messing with docker-compose and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Fix ownership of /app directory and its contents
+chown devuser:devuser /app
+chown -R devuser:devuser /app
+
 # Switch to devuser and run the command
 # If no arguments provided, default to zsh
 if [ $# -eq 0 ]; then
